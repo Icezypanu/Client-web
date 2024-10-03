87b1ea3208803c2364346128d98067b48b0a3bf4
Panupong Kiaengkham <68285395+Icezypanu@users.noreply.github.com>
2024-10-03 13:51:58 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import IndexJS from './client/src/index'
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <IndexJS />
+  </React.StrictMode>
+);
+
+reportWebVitals();
